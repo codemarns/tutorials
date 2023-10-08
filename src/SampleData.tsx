80be74e757c572ee9ec584ce48e4fa3cab16abcf
Marnien Cueba <marniencueba94@gmail.com>
2023-10-08 14:34:43 +0800

    add: sample data

---
+++ src/SampleData.tsx
@@ -0,0 +1,60 @@
+const SampleData = () => {
+  return (
+    <section id="section-1">
+      Aute nulla ad duis nulla minim ea in sit mollit do voluptate ex. Sunt
+      ipsum minim non deserunt. Magna proident ad id occaecat Lorem eu ullamco
+      reprehenderit ipsum exercitation deserunt occaecat fugiat. Nisi veniam do
+      cupidatat cillum incididunt deserunt non est id. Laboris Lorem Lorem
+      occaecat ipsum eu labore cillum minim minim. Aliqua laboris ad ipsum amet
+      Lorem sit fugiat do. Ipsum velit aute occaecat sit quis elit anim. Sit
+      ipsum ut amet in duis. Minim veniam ea elit ullamco ut adipisicing dolor
+      in. Deserunt reprehenderit cillum ex sit adipisicing elit veniam aliquip.
+      Proident deserunt reprehenderit id cillum dolor amet tempor aute labore
+      nisi enim. Velit Lorem incididunt in mollit laborum esse aliquip aute
+      laborum nulla incididunt. Ea cupidatat ut culpa eu exercitation aute
+      veniam. Id velit reprehenderit tempor eu id nostrud enim sit ex laboris
+      nulla officia exercitation cillum. Ex irure cillum do qui pariatur.
+      Pariatur aliqua mollit sunt qui et dolore duis duis irure. Est consequat
+      deserunt id eiusmod velit. Amet dolore nisi et velit. Cillum elit laboris
+      minim consectetur sunt. Occaecat in cupidatat voluptate mollit sunt
+      commodo est eiusmod irure. Ex anim proident Lorem anim dolor Lorem commodo
+      exercitation. Voluptate aliqua laboris non tempor labore elit. Tempor eu
+      nulla mollit dolore sunt minim adipisicing excepteur ut. Labore cillum
+      sunt qui ea est cillum eu. Tempor cillum aute minim aliqua Lorem. Ad do
+      reprehenderit quis in minim duis occaecat ullamco adipisicing consequat
+      mollit aute. Proident anim elit incididunt minim. Incididunt officia
+      tempor adipisicing aliquip laborum eiusmod voluptate nostrud mollit
+      laboris Lorem est sit. Irure qui esse labore eiusmod nulla aute occaecat
+      aliquip pariatur est deserunt reprehenderit officia. Nulla dolore eu
+      laboris enim nisi consequat laboris ullamco. Elit magna nulla adipisicing
+      nostrud officia ullamco commodo reprehenderit quis do voluptate. Laboris
+      nostrud commodo id culpa irure dolore ea ea nulla. Laborum officia quis
+      labore enim velit in velit deserunt. Officia ea dolor anim est dolor
+      pariatur aliqua veniam sint eu ipsum mollit velit. Sunt et qui consequat
+      aute do deserunt id esse ex velit. Cupidatat ex dolore aliquip aliqua amet
+      pariatur nisi velit sit ullamco nisi. Excepteur esse magna cupidatat
+      nulla. In ex ex in consectetur incididunt laboris sunt laboris tempor
+      duis. Incididunt deserunt duis pariatur adipisicing id cillum. Commodo non
+      tempor culpa aute. Id mollit commodo anim laborum. Ipsum velit incididunt
+      in incididunt occaecat nulla culpa veniam commodo nulla aliquip sint est.
+      Dolore ut ipsum duis nisi do cupidatat. Aliqua in do nulla irure velit est
+      voluptate ex irure Lorem. Lorem veniam fugiat esse ipsum sit veniam ad
+      velit. Nisi ea aliquip pariatur consequat. Proident ad ex eiusmod aute id
+      elit id magna enim aliqua excepteur adipisicing. Adipisicing ea laboris
+      magna voluptate nostrud qui adipisicing aute sunt deserunt anim consequat.
+      Nostrud reprehenderit nulla ut adipisicing nulla. Minim dolor sint irure
+      tempor excepteur deserunt ut eiusmod eu non duis cillum incididunt amet.
+      Sint fugiat magna qui exercitation irure velit sunt Lorem esse mollit esse
+      ullamco mollit minim. Aute excepteur officia non magna reprehenderit ex.
+      Pariatur tempor sit veniam duis aliqua commodo velit. Est occaecat nostrud
+      sit commodo ut esse culpa labore sit culpa amet. Amet ut laboris nulla in
+      proident consequat ullamco excepteur nisi ipsum. Officia tempor anim
+      adipisicing exercitation deserunt eiusmod irure cupidatat non. Elit
+      adipisicing aliqua sint aliquip exercitation nisi minim mollit. Aliquip
+      culpa consequat eu non duis exercitation in enim mollit commodo veniam in
+      occaecat. Et labore nostrud aliquip anim.
+    </section>
+  );
+};
+
+export default SampleData;
