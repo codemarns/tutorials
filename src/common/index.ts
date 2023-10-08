36612ec41db11977edf49ed746cc41fc79887825
Marnien Cueba <marniencueba94@gmail.com>
2023-10-08 17:38:00 +0800

    update: common file

--- src/common/index.ts
+++ src/common/index.ts
@@ -1,3 +1,6 @@
+export { Header } from "./Header";
+export { Wrapper } from "./Wrapper";
 export { Sidebar } from "./Sidebar";
 export { Container } from "./Container";
+export { Content } from "./Content";
 export { Panel } from "./Panel";
