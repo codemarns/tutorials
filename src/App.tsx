70196c8f983dfa98b75cccac70c645bee31605f9
Marnien Cueba <marniencueba94@gmail.com>
2023-10-08 18:03:11 +0800

    cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,10 @@
 import cn from "classnames";
-import { useEffect, useState } from "react";
 import { Container, Content, Header, Panel, Sidebar, Wrapper } from "./common";
 import { mainStyles as styles } from "./styles";
 import SampleData from "./SampleData";
 
 function App() {
   const codename = "codemarns";
-  const [screenSize, setScreenSize] = useState(getCurrentDimension());
-
-  function getCurrentDimension() {
-    return {
-      width: window.innerWidth,
-      height: window.innerHeight,
-    };
-  }
-
-  useEffect(() => {
-    const updateDimension = () => {
-      setScreenSize(getCurrentDimension());
-    };
-
-    window.addEventListener("resize", updateDimension);
-    return () => {
-      window.removeEventListener("resize", updateDimension);
-    };
-  }, [screenSize]);
 
   return (
     <main
