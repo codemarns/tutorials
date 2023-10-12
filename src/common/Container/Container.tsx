7dadad0494fa47233d3338c2193485dfd6b12ecb
Marnien Cueba <marniencueba94@gmail.com>
2023-10-12 18:49:04 +0800

    update: Container styles

--- src/common/Container/Container.tsx
+++ src/common/Container/Container.tsx
@@ -2,16 +2,17 @@ import React, { useContext } from "react";
 import cn from "classnames";
 import { Context } from "../../App";
 import { Card } from "../../components";
+import { containerStyles } from "../../styles";
 
 type ContainerProps = React.HTMLAttributes<HTMLDivElement>;
 
 const Container: React.FC<ContainerProps> = (props) => {
   const { title, children } = props;
   const { codename } = useContext(Context);
-
+  const { container } = containerStyles;
   return (
     <Card
-      className={cn({ [codename + "__container"]: codename }, "min-h-full")}
+      className={cn({ [codename + "__container"]: codename }, container.base)}
       title={title}
       id={"container"}
       actions={[]}
