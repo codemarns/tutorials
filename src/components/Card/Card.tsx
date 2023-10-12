cb9a6423d49099fcb5d59095e96a182fbad90558
Marnien Cueba <marniencueba94@gmail.com>
2023-10-12 18:53:19 +0800

    update: styling and remove direct styles

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,11 +1,11 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
-import React from "react";
+import React, { ReactElement } from "react";
 import cn from "classnames";
-import { cardStyles as styles } from "./styles";
+import { cardStyles } from "../../styles";
+// import { cardStyles as styles } from "./styles";
 
 type Actions = {
   id: string;
-  icon?: any;
+  icon?: ReactElement;
   onClick?: () => void;
 };
 
@@ -26,26 +26,27 @@ const Card: React.FC<CardProps> = (props) => {
 
   const COMPONENT_NAME = "cm-card";
 
+  const { card } = cardStyles;
   return (
     <div
       {...restProps}
       id={id}
-      className={cn(COMPONENT_NAME, styles.base, className)}
+      className={cn(COMPONENT_NAME, card.base, className)}
     >
       {title || actions?.length > 0 ? (
         <div
           className={cn(
             { [COMPONENT_NAME + "__header"]: COMPONENT_NAME },
-            styles.header.base
+            card.header.base
           )}
         >
-          <span className={styles.header.title}>{title}</span>
-          <div className={styles.header.actions.base}>
+          <span className={card.header.title.base}>{title}</span>
+          <div className={card.header.actions.base}>
             {actions?.map((item, index) => (
               <div
                 key={index}
                 id={item.id}
-                className={styles.header.actions.item}
+                className={card.header.actions.item.base}
               />
             ))}
           </div>
@@ -58,7 +59,7 @@ const Card: React.FC<CardProps> = (props) => {
             { [COMPONENT_NAME + "__content"]: COMPONENT_NAME },
             "content-with-scroll",
             title || actions?.length > 0 ? "h-[calc(100%-3rem)]" : "h-full",
-            styles.content.base
+            card.content.base
           )}
         >
           {children}
